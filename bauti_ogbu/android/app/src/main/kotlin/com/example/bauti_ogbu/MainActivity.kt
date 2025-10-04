d2aeb8b610a569da790609fc1b05fbc6e64bde03
nivek's <150204523+frank777ch@users.noreply.github.com>
2025-10-04 00:16:48 -0500

    Add initial Flutter project scaffold
    Bootstrap new Flutter project 'bauti_ogbu' with default configuration, platform-specific files for Android, iOS, Linux, macOS, web, and Windows, as well as initial Dart analysis options and a sample test. This sets up the project structure and build files for cross-platform development.

---
+++ bauti_ogbu/android/app/src/main/kotlin/com/example/bauti_ogbu/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bauti_ogbu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- dart_examples/04.dart
+++ dart_examples/04.dart
@@ -8,11 +8,35 @@
   print('index 3: ${numbers[3]} ');
   print('First: ${numbers.first} ');
   print('Last: ${numbers.last} ');
-  print('Reversed: ${numbers.reversed} ');
 
   print('Tipo de datos: ${numbers.runtimeType} ');
 
+  print('----------------------------------');
+
   final reversedNumbers = numbers.reversed;  
 
-  print('Tipo de datos: ${reversedNumbers.runtimeType} ');
+  print('Reversed numbers: $reversedNumbers ');
+
+  print('Reversed numbers: ${reversedNumbers.runtimeType} ');
+
+  print('----------------------------------');
+
+  print(reversedNumbers.toList());
+    print(reversedNumbers.toList().runtimeType);
+
+    print('----------------------------------');
+
+  print(reversedNumbers.toSet());
+    print(reversedNumbers.toSet().runtimeType);
+
+  print('----------------------------------');
+
+  print(reversedNumbers.toString());
+    print(reversedNumbers.toString().runtimeType);
+
+  print('----------------------------------');
+
+  print(reversedNumbers.toSet().toList());
+    print(reversedNumbers.toSet().toList().runtimeType);
+
 }
